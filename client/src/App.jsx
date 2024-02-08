b557da714162b7ef5ce0be42400aa590dac4bd0a
Vishal21121 <vs84077000@gmail.com>
2024-02-08 18:27:19 +0530

    added: Signup.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import Home from "./Pages/Home"
 import Sigin from "./Pages/Sigin"
+import Signup from "./Pages/Signup"
 
 function App() {
 
   return (
     <>
       {/* <Home /> */}
-      <Sigin />
+      {/* <Sigin /> */}
+      <Signup />
     </>
   )
 }
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import sigin from "../assets/sigin.svg"
+import signup from "../assets/signup.svg"
 
-const Sigin = () => {
+const Signup = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src={sigin} alt="" />
-                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 mr-4">
+            <div className="hero-content flex gap-8 lg:flex-row-reverse">
+                <img src={signup} alt="" />
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Username</span>
+                            </label>
+                            <input type="text" placeholder="username" className="input input-bordered" required />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -21,11 +27,7 @@ const Sigin = () => {
                             <input type="password" placeholder="password" className="input input-bordered" required />
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Sign in</button>
-                        </div>
-                        <div className='flex gap-2 mx-auto'>
-                            <span>Don't have an account?</span>
-                            <a href="" className='hover:text-blue-500'>Sign Up</a>
+                            <button className="btn btn-primary">Sign up</button>
                         </div>
                     </form>
                 </div>
@@ -34,4 +36,4 @@ const Sigin = () => {
     )
 }
 
-export default Sigin
+export default Signup
