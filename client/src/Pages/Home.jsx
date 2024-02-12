c456ca1c9357eb922a75c7807f392d25f78f7f00
Vishal21121 <vs84077000@gmail.com>
2024-02-12 20:57:35 +0530

    Add useEffect hook and fetchUrls function to fetch and display saved URLs

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { IoIosLink } from "react-icons/io";
 import { FaCopy } from "react-icons/fa6";
 import { toast } from 'react-toastify';
@@ -9,13 +9,13 @@ import UrlCard from '../components/UrlCard';
 
 const Home = () => {
     const { user } = useUserContext()
-    console.log(user)
     const [data, setData] = useState({
         longURL: "",
         aliase: ""
     })
     const [gotURL, setGotURL] = useState(false)
     const [shortURL, setShortURL] = useState("")
+    const [urls, setUrls] = useState([])
 
     const handleClick = async () => {
         if (!validurl.isUri(data.longURL)) {
@@ -39,6 +39,7 @@ const Home = () => {
             if (value.data.statusCode === 201) {
                 setGotURL(true)
                 setShortURL(value.data.value.shortUrl)
+                setUrls((pre) => [...pre, value.data.value])
                 toast.success('Created a short URL');
             } else if (value.data.statusCode === 400) {
                 toast.error(value.data.message);
@@ -59,13 +60,31 @@ const Home = () => {
         }
     }
 
+    const fetchUrls = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/v1/url/${user.data.data._id}`)
+            const data = await response.json()
+            console.log(data)
+            if (data.data.statusCode === 200) {
+                setUrls(data.data.value)
+                console.log("data GIt", data.data.value)
+            }
+        } catch (error) {
+            console.log(error.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchUrls()
+    }, [])
+
     return (
         <div className='flex flex-col w-full items-center h-screen'>
             <div className="navbar bg-neutral text-neutral-content w-full">
                 <button className="btn btn-ghost text-xl">URL Shortner</button>
             </div>
             <div className='flex gap-4 w-full h-[80%]'>
-                <div className='w-1/2 mt-8 flex justify-center max-h-screen h-[50%]'>
+                <div className='w-1/2 mt-8 flex justify-center max-h-screen h-fit'>
                     <div className='ring-2 ring-gray-600 mt-8 ml-8 px-4 gap-4 w-3/4 flex flex-col rounded pb-4 pt-2 h-full'>
                         <div className='flex gap-4 p-2 items-center'>
                             <IoIosLink className='text-white text-4xl' />
@@ -102,12 +121,14 @@ const Home = () => {
                 </div>
                 <div className='w-1/2 p-4 h-full flex flex-col gap-4'>
                     <p className='text-2xl text-center font-semibold'>Saved Urls</p>
-                    <div className='flex flex-col gap-4 p-4 overflow-auto h-full'>
-                        <UrlCard />
-                        <UrlCard />
-                        <UrlCard />
-                        <UrlCard />
-                        <UrlCard />
+                    <div className='flex flex-col gap-4 p-4 overflow-auto h-full items-center w-full'>
+                        {
+                            urls.length > 0 ? (
+                                urls.map((el) => (
+                                    <UrlCard key={el._id} el={el} />
+                                ))
+                            ) : <p>Nothing to display</p>
+                        }
                     </div>
                 </div>
             </div>
--- client/src/components/UrlCard.jsx
+++ client/src/components/UrlCard.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { FaCopy } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 
 
-const UrlCard = () => {
+const UrlCard = ({ el }) => {
+    console.log(el)
+    let dateStr = el.createdAt
+    let date = new Date(dateStr);
+    let options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+    };
+    let formattedDate = date.toLocaleDateString('en-US', options);
+
+
     return (
-        <div className='flex flex-col gap-2 ring ring-gray-500 rounded p-2'>
-            <p className='text-gray-400 font-bold text-lg'>ShortUrl</p>
-            <p>LongURL</p>
+        <div className='flex flex-col gap-2 ring ring-gray-500 rounded p-2 w-3/4'>
+            <Link className='text-gray-400 font-bold text-lg' to={el.shortUrl} target="_blank"  >{el.shortUrl}</Link>
+            <p>{el.redirectUrl}</p>
             <div className='flex gap-2'>
-                <p>0 Count</p>
-                <p>Time</p>
+                <p>{el.clicked} clicks</p>
+                <p>{formattedDate}</p>
             </div>
-            <button className='btn btn-active text-lg w-1/4' onClick={async () => await navigator.clipboard.writeText(shortURL)}><FaCopy className='text-2xl' />Copy</button>
+            <button className='btn btn-active text-lg w-1/4' onClick={async () => await navigator.clipboard.writeText(el.shortUrl)}><FaCopy className='text-2xl' />Copy</button>
         </div>
     )
 }
