6be8e2bc836aaf3d9135e75a058333d1a9e05de4
Vishal21121 <vs84077000@gmail.com>
2024-03-25 14:13:28 +0530

    added: client_uri in origin in cors

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser())
 // app.use(express.static("./dist"))
 app.use(cors(
     {
-        origin: ['http://localhost:5173'],
+        origin: ['http://localhost:5173', process.env.CLIENT_URI],
         credentials: true,
         optionsSuccessStatus: 200
     }
